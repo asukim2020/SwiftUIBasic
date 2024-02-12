2dfe29012b5fe5319cc5442eb1ed8d77c1aa6b24
asukim <asukim2020@gmail.com>
2024-02-12 21:03:51 +0900

    first commit

---
+++ SwiftUIBasic/TextBasic.swift
@@ -0,0 +1,18 @@
+//
+//  TextBasic.swift
+//  SwiftUIBasic
+//
+//  Created by 김현구 on 2/12/24.
+//
+
+import SwiftUI
+
+struct TextBasic: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TextBasic()
+}
